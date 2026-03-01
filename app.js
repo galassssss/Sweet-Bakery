08eba903593cef22ed1143827770acc209236342
galassssss <vungocminhhoang095@gmail.com>
2026-03-01 16:10:54 +0700

    add sweet

---
+++ app.js
@@ -0,0 +1,4 @@
+const path = location.pathname.split("/").pop() || "index.html";
+document.querySelectorAll(".nav a").forEach(a => {
+  if (a.getAttribute("href") === path) a.classList.add("active");
+});
